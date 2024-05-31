19673ed6901be28bba228846e1294d8e9b1ddeee
Charles Joshua Uy <129761131+CJ-Uy-EVC@users.noreply.github.com>
2024-06-01 03:13:08 +0800

    github pages depolyment

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  mode: "static",
+  router: {
+    base: "/DNA-Barcode-Generator/",
+  },
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
-})
+  modules: ["@nuxt/ui"],
+});
