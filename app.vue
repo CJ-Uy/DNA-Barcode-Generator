557001a6b9b8042eb62317fdbaad0ef450699f1c
Charles Joshua Uy <129761131+CJ-Uy-EVC@users.noreply.github.com>
2024-06-05 00:20:39 +0800

    Added legend

--- app.vue
+++ app.vue
@@ -3,6 +3,7 @@ import html2canvas from "html2canvas";
 
 const dnaSequence = ref("");
 const barcodeWidth = ref(500);
+
 const colorMapping = {
   a: "green",
   c: "blue",
@@ -22,9 +23,17 @@ const barcodeColors = ref([]);
 
 const barcodeElement = ref(null);
 const downloadBarcode = async () => {
+  window.scrollTo(0, 0);
   // Check if barcodeElement.value is a valid HTML element
   if (barcodeElement.value instanceof HTMLElement) {
-    const canvas = await html2canvas(barcodeElement.value);
+    const canvas = await html2canvas(barcodeElement.value, {
+      width: barcodeElement.value.scrollWidth,
+      height: barcodeElement.value.scrollHeight,
+      windowWidth: barcodeElement.value.scrollWidth,
+      windowHeight: barcodeElement.value.scrollHeight,
+      scrollY: -window.scrollY,
+      scale: 1, // You can adjust the scale for better resolution
+    });
     const image = canvas.toDataURL("image/png");
 
     // Create a link to download the image
@@ -74,12 +83,30 @@ const downloadBarcode = async () => {
         ref="barcodeElement"
         :style="{ width: barcodeWidth + 'px' }"
       >
+        <!-- Barcode -->
         <div
           v-for="(color, index) in barcodeColors"
           :key="index"
           :style="{ backgroundColor: color }"
-          class="w-1 h-10 mr-[1px] inline-block"
+          class="w-1 h-10 mr-[1px] inline-block mb-[-3.5px]"
         ></div>
+      </div>
+      <div>
+        <!-- Legend -->
+        <div class="text-center mt-3 font-bold">LEGEND:</div>
+        <div class="legend flex justify-center mt-3">
+          <div
+            v-for="(color, nucleotide) in colorMapping"
+            :key="nucleotide"
+            class="flex items-center mr-12"
+          >
+            <div :style="{ backgroundColor: color }" class="w-1 h-6 mr-2"></div>
+            <span v-if="nucleotide != 'default'">{{
+              nucleotide.toUpperCase()
+            }}</span>
+            <span v-else>No Consensus</span>
+          </div>
+        </div>
       </div>
     </div>
   </div>
