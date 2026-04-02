77f0dada57ca398801de9e468d12105c0097f1e2
CJ-Uy <75460325+CJ-Uy@users.noreply.github.com>
2026-04-02 18:07:52 +0800

    new favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  app: {
+    head: {
+      link: [{ rel: 'icon', type: 'image/jpeg', href: '/favicon.jpg' }],
+    },
+  },
   devtools: { enabled: true },
   modules: ['@nuxt/ui'],
   css: ['~/assets/css/main.css'],
